7871580b8b95fa7694ac0ddcbcc8beb18b38ef67
Kodey Converse <kodey@krconv.com>
2017-09-05 19:38:41 -0400

    Create task to automate sass compilation

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// Sass configuration
+var gulp = require('gulp'),
+sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(function (f) {
+            return 'public/css/';
+        }))
+});
+
+gulp.task('default', ['sass'], function () {
+    gulp.watch('style.scss', ['sass']);
+})
