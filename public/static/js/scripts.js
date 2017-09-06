4786698d034ebd96a3f2e6e480c2856f6b9ec8e3
Kodey Converse <kodey@krconv.com>
2017-09-06 09:25:22 -0400

    Finish layout for projects and contact pages

--- public/static/js/scripts.js
+++ public/static/js/scripts.js
@@ -1,16 +1,19 @@
-$(document).ready(function() {
-    loadPage(document.location.pathname);
+$(document).ready(function () {
+    $('main, nav').css({ opacity: 0 });
+    loadPage(document.location.pathname, function () {
+        $('main, nav').animate({ opacity: 1 }, 300);
+    });
 
-    $('nav > a').click(function(event) {
+    $('nav > a').click(function (event) {
         window.history.pushState({}, document.title, $(this).attr('href'));
         loadPage(document.location.pathname);
-        
+
         event.preventDefault();
     });
 });
 
-function loadPage(page) {
+function loadPage(page, callback = null) {
     if (page == '/')
         page = '/about'
-    $('main').load('/page' + page);
+    $('main').load('/page' + page, callback);
 }
