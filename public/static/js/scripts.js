7f54cd7e04f5c75987d9779b8bd13fc44ca1146b
Kodey Converse <kodey@krconv.com>
2017-09-06 12:08:02 -0400

    Add color and minor adjustments

--- public/static/js/scripts.js
+++ public/static/js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     $('main, nav').css({ opacity: 0 });
     loadPage(document.location.pathname, function () {
-        $('main, nav').animate({ opacity: 1 }, 300);
+        $('main, nav').animate({ opacity: 1 }, 200, function() {
+            $('main, nav').css('opacity', '');
+        });
     });
 
     $('nav > a').click(function (event) {
-        window.history.pushState({}, document.title, $(this).attr('href'));
-        loadPage(document.location.pathname);
+        if (window.location.pathname != $(this).attr('href')) {
+            window.history.pushState({}, document.title, $(this).attr('href'));
+            loadPage(document.location.pathname);
+        }
 
         event.preventDefault();
     });
@@ -15,5 +19,11 @@ $(document).ready(function () {
 function loadPage(page, callback = null) {
     if (page == '/')
         page = '/about'
+    $('nav > a').each(function() {
+        if ($(this).attr('href') == page)
+            $(this).addClass('active');
+        else
+            $(this).removeClass('active');
+    })
     $('main').load('/page' + page, callback);
 }
