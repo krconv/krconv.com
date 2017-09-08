a190aa0a59e11860feda54d61a4494380462c621
Kodey Converse <kodey@krconv.com>
2017-09-08 02:03:09 -0400

    Change port to 80

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var url = require('url'),
   favicon = require('serve-favicon'),
   path = require('path'),
   fs = require('fs'),
-  port = process.env.PORT || 8080;
+  port = process.env.PORT || 80;
 
 const app = express();
 app.use('/static/', express.static('public/static'));
